7aad5b30b3a72ce9a6038d3130db04e5b686d53f
tidika <tochi.idika@gmail.com>
2025-09-04 09:29:32 -0500

    updated files

--- frontend/app.py
+++ frontend/app.py
@@ -22,9 +22,9 @@ try:
     if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
         bedrock_agent_runtime = boto3.client(
             service_name='bedrock-agent-runtime',
-            region_name=AWS_REGION,
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+            region_name=AWS_REGION
+            # aws_access_key_id=AWS_ACCESS_KEY_ID,
+            # aws_secret_access_key=AWS_SECRET_ACCESS_KEY
         )
     else:
         bedrock_agent_runtime = boto3.client(
